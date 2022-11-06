f7619bb4da40c8dce01fe120a04d46e82a875758
abin john <abinpj007@gmail.com>
2022-11-06 23:02:38 +0000

    changes

--- src/test/java/com/mystore/test/AccountCreationPageTest.java
+++ src/test/java/com/mystore/test/AccountCreationPageTest.java
@@ -15,11 +15,11 @@ public class AccountCreationPageTest extends BaseClass{
 	LoginPage LoginPageob;
 	AccountCreationPage accountPageob;
 	@Test(dataProvider="email" ,dataProviderClass=DataProviders.class,groups="Sanity")
-	public void verifyCreateAccountPageTest(String neEmail) {
+	public void verifyCreateAccountPageTest(String newEmail) {
 		inde=PageFactory.initElements(driver,IndexPage.class);
 		inde.clickOnSign();
 		LoginPageob=PageFactory.initElements(driver,LoginPage.class);
-		LoginPageob.createNewAccount(neEmail);
+		LoginPageob.createNewAccount(newEmail);
 		accountPageob=PageFactory.initElements(driver,AccountCreationPage.class);
 		boolean result=accountPageob.validateAcountCreatePage();
 		Assert.assertTrue(result);
